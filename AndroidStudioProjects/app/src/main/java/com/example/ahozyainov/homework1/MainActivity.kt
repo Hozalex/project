b108e419d8d15dbad77817c3e11d2f201673038d
Khozyainov Aleksandr <hozalex@gmail.com>
2018-05-23 11:24:38 +0300

    weather app

--- AndroidStudioProjects/app/src/main/java/com/example/ahozyainov/homework1/MainActivity.kt
+++ AndroidStudioProjects/app/src/main/java/com/example/ahozyainov/homework1/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.ahozyainov.homework1
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Spinner
@@ -10,30 +11,70 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private var city: String? = null
+    private val CITY_NAME: String = ""
+    private lateinit var settings: SharedPreferences
+    private lateinit var spinner: Spinner
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var weatherString: Array<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var spinner: Spinner = findViewById(R.id.cities_spinner)
-        var button: Button = findViewById(R.id.check_button)
-        var textView: TextView = findViewById(R.id.text_view)
-        var imageView: ImageView = findViewById(R.id.image)
-        var weatherString: Array<String> = resources.getStringArray(R.array.weather)
+        settings = getSharedPreferences("mySettings", Context.MODE_PRIVATE)
+        spinner = findViewById(R.id.cities_spinner)
+        button = findViewById(R.id.check_button)
+        textView = findViewById(R.id.text_view)
+        imageView = findViewById(R.id.image)
+        weatherString = resources.getStringArray(R.array.weather)
 
-        button.setOnClickListener(View.OnClickListener {
-            var position: Int = spinner.selectedItemPosition
-            if (position == 0 || position == 3) {
-                imageView.setImageResource(R.drawable.sunny)
-                textView.text = weatherString[1]
+        checkSettings()
 
-            } else if (position == 1 || position == 2) {
-                imageView.setImageResource(R.drawable.rainy)
-                textView.text = weatherString[0]
-            }
-
-
+        button.setOnClickListener({
+            checkWeather()
         })
 
     }
+
+    override fun onPause() {
+        super.onPause()
+        city = spinner.selectedItem.toString()
+        settings.edit().putString(CITY_NAME, city).apply()
+
+    }
+
+    private fun checkWeather() {
+        var position: Int = spinner.selectedItemPosition
+
+        if (position == 0 || position == 3) {
+            imageView.setImageResource(R.drawable.sunny)
+            textView.text = weatherString[1]
+            textView.setBackgroundResource(R.color.colorBackground)
+
+        } else if (position == 1 || position == 2) {
+            imageView.setImageResource(R.drawable.rainy)
+            textView.text = weatherString[0]
+            textView.setBackgroundResource(R.color.colorBackground)
+        }
+
+    }
+
+    private fun checkSettings() {
+        if (settings.contains(CITY_NAME)) {
+            var checkCity: String = settings.getString(CITY_NAME, "")
+            var cityArray: Array<String> = resources.getStringArray(R.array.cities)
+            var index: Int = 0
+            for (item in cityArray) {
+                if (checkCity.equals(item)) {
+                    index = cityArray.indexOf(item)
+                }
+            }
+            spinner.setSelection(index)
+        }
+    }
+
 }
